eeca23625d7f4bce436dc0cbffb607df359df1e7
Hojin Jang <onewithtruth@gmail.com>
2022-06-22 16:53:50 +0800

    initial commit

---
+++ src/main/java/com/hands/comongjava/ComongjavaApplication.java
@@ -0,0 +1,13 @@
+package com.hands.comongjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComongjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComongjavaApplication.class, args);
+	}
+
+}
