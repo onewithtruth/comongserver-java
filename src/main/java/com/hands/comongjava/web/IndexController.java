1c7371d0312775da5aaaf94b3c87dc6f4351d9d5
Hojin Jang <onewithtruth@gmail.com>
2022-06-24 16:50:05 +0800

    initial commit

--- src/main/java/com/hands/comongjava/web/IndexController.java
+++ src/main/java/com/hands/comongjava/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.hands.comongjava.web;
 
+import com.hands.comongjava.config.auth.dto.SessionUser;
 import com.hands.comongjava.service.posts.PostsService;
 import com.hands.comongjava.web.dto.PostResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,26 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession
+                .getAttribute("user");
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
