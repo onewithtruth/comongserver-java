ce8c3f2595c6911d1300c2066bcaac53022e52bc
Hojin Jang <onewithtruth@gmail.com>
2022-06-25 17:36:58 +0800

    initial commit

--- src/main/java/com/hands/comongjava/config/SecurityConfig.java
+++ src/main/java/com/hands/comongjava/config/SecurityConfig.java
@@ -32,6 +32,7 @@ public class SecurityConfig {
 //                .userService(customOAuth2UserService);
 //    }
 
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
