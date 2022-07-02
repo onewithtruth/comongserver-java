96323d6c3f50f7bd3c23ad525637647a42a202db
Hojin Jang <onewithtruth@gmail.com>
2022-07-02 17:25:42 +0800

    deploy script files update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-BUILD_JAR=$(ls /home/ubuntu/comongjava/build/libs/practice-githubAction-deploy-0.0.1-SNAPSHOT.jar)
+BUILD_JAR=$(ls /home/ubuntu/comongjava/build/libs/comongjava-0.0.1-SNAPSHOT.jar)
 JAR_NAME=$(basename $BUILD_JAR)
 
 echo "> 현재 시간: $(date)" >> /home/ubuntu/comongjava/deploy.log
